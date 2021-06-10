2de19ed25fb1e82f6b7551a795cd85d9ffc4f066
Adrian Hurtado <ahurta92@gmail.com>
2021-06-09 22:02:49 -0400

    use the new harvester function

--- orb_analysis.py
+++ orb_analysis.py
@@ -4,12 +4,15 @@ from orb_func import read_orb_plots
 from orb_func import read_ground_orb_plots
 from orb_func import get_num_states
 from orb_func import compute_energies
-from orb_func import make_plots
-import matplotlib.pyplot as plt
+from read_output import harvest_outfile_pass
+
 
 test = "static"
-#test = "dynamic_p1"
-#test = "dynamic_p2"
+response_file = test + "/response.out"
+psivar = harvest_outfile_pass(response_file)
+o_e = psivar["ORBITAL ENERGIES"]
+# test = "dynamic_p1"
+# test = "dynamic_p2"
 
 # ground plots
 [num_states, num_orbs] = get_num_states(test)
@@ -29,14 +32,14 @@ x_z = read_orb_plots("x", test, "z", num_states, num_orbs)
 plot_path = "plots/" + test + "/"
 lo_val = 0.0005
 hi_val = 0.0009
-p_lo=0
-p_hi=24
+p_lo = 0
+p_hi = 24
 
-#plot_x_x = make_plots(x_x, plt.plot, p_lo, p_hi, plot_path)
-#plot_y_x = make_plots(y_x, plt.plot, p_lo, p_hi,plot_path)
+# plot_x_x = make_plots(x_x, plt.plot, p_lo, p_hi, plot_path)
+# plot_y_x = make_plots(y_x, plt.plot, p_lo, p_hi,plot_path)
 
 # e_xx_df = compute_orbital_energies(x_x, lo, hi, plot_path)
 # e_yx_df = compute_orbital_energies(y_x, lo, hi, plot_path)
-e_gx_df = compute_energies(g_x, lo_val, hi_val, p_hi, plot_path)
-e_xx_df = compute_energies(x_z, lo_val, hi_val,  p_hi, plot_path)
-g_x0=g_x.loc['ground_x_0'][0]
+e_gx_df = compute_energies(g_x, lo_val, hi_val, p_hi, plot_path,o_e )
+e_xx_df = compute_energies(x_z, lo_val, hi_val, p_hi, plot_path,o_e )
+g_x0 = g_x.loc["ground_x_0"][0]
