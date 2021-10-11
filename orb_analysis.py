d9bb9ea648ce3efcfefacdf1b68abbc5a2fd83b2
Adrian Hurtado <ahurta92@gmail.com>
2021-10-11 13:23:10 -0400

    density plots

--- orb_analysis.py
+++ orb_analysis.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 # from orb_func import read_orb_plots
-from orb_func import read_ground_orb_plots
-from orb_func import get_num_states
-from orb_func import read_ground_density_plots
-from orb_func import compute_energies
-from orb_func import read_transition_density_plot
+from orb_func import read_density_plots
+from orb_func import compute_energies_density
 from read_output import harvest_outfile_pass
 import glob
 
@@ -22,39 +19,25 @@ tests = glob.glob(molecule + "/o_*")
 response_file = response_dir + "/response.out"
 print(response_file)
 
-# psivar = harvest_outfile_pass(response_file)
-# o_e = psivar["ORBITAL ENERGIES"]
+psivar = harvest_outfile_pass(response_file)
+o_e = psivar["ORBITAL ENERGIES"]
+num_states = int(psivar["NUM STATES"])
+num_orbitals = int(psivar["NUM ORBITALS"])
+num_orders=2
 
 # ground plots
-[num_states, num_orbs] = get_num_states(response_dir)
+# [num_states, num_orbs] = get_num_states(response_dir)
 
-rho_0 = read_ground_density_plots(response_dir)
-## read in transition density plots (
-rho_1x = read_transition_density_plot(response_dir, num_states, "x")
-rho_1y = read_transition_density_plot(response_dir, num_states, "y")
-rho_1z = read_transition_density_plot(response_dir, num_states, "z")
-# g_x = read_ground_orb_plots(num_orbs, response_dir, "x")
-# g_y = read_ground_orb_plots(num_orbs, response_dir, "y")
-# g_z = read_ground_orb_plots(num_orbs, response_dir, "z")
 
-# direction x (1 state m oritals
-# x_x = read_orb_plots("x", response_dir, "x", num_states, num_orbs)
-# y_x = read_orb_plots("y", response_dir, "x", num_states, num_orbs)
-
-# direction y
-# x_y = read_orb_plots("x", response_dir, "y", num_states, num_orbs)
-# y_y = read_orb_plots("y", response_dir, "y", num_states, num_orbs)
-# direction z
-# y_z = read_orb_plots("y", response_dir, "z", num_states, num_orbs)
-# x_z = read_orb_plots("x", response_dir, "z", num_states, num_orbs)
-
-# plot_path = response_dir + "/plots"
+rho_x = read_density_plots(response_dir,num_orders , num_states)
+plot_path = response_dir + "/plots"
 
 lo_val = 0.02
 hi_val = 0.025
 p_lo = 0
 p_hi = 24
 
+e_rho_x = compute_energies_density(rho_x, num_orders,num_states,lo_val, hi_val, p_hi, plot_path)
 # e_xx_df = compute_orbital_energies(x_x, lo, hi, plot_path)
 # e_yx_df = compute_orbital_energies(y_x, lo, hi, plot_path)
 # e_gx_df = compute_energies(g_x, lo_val, hi_val, p_hi, plot_path, o_e)
--- orb_func.py
+++ orb_func.py
@@ -5,21 +5,127 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 
 
-def read_ground_orb_plots(num_orbitals, test, di):
-    plot_path = test + "/plots/ground/"
-    o_dict = {}
+# return dataframe of densities for given direction
+# order by states
+# zero order has a single state
+# all other order has num_states
+#
+def read_density_plots(response_dir, num_orders, num_states):
+    plot_path = response_dir + "/plots/densities/"
 
-    cols = []
-    for j in range(num_orbitals):
-        cols.append(str(j))
-    for i in range(1):
-        p_1 = "ground_" + di + "_" + str(i)
-        o_dict[p_1] = []
-        for j in range(num_orbitals):
-            p_2 = "ground_" + di + "_" + str(j)
-            p_3 = plot_path + p_2 + ".plt"
-            o_dict[p_1].append(np.genfromtxt(p_3, dtype=float))
-    return pd.DataFrame.from_dict(o_dict, orient="index", columns=cols)
+    cols = ["x", "y", "z"]
+    idx = ["00"]
+    for o in range(1, num_orders):
+        for states in range(num_states):
+            idx.append(str(o) + str(states))
+
+    print(idx)
+    densities_df = pd.DataFrame(index=idx, columns=cols)
+
+    for o in idx:
+        for di in cols:
+            order = o[0]
+            state = o[1]
+            p_path = "rho" + order + "_" + di + "_" + state + ".plt"
+            p_3 = plot_path + p_path
+            print(p_3)
+            densities_df.loc[o, di] = np.genfromtxt(p_3, dtype=float)
+    return densities_df
+
+
+# TODO figure out fit to exp(-alpha r) alpha=sqrt(-2*e_i)
+# exp(-alpha*r)/r^2
+def compute_energies_density(df, num_orders, num_states, lo_val, hi_val,
+                             center_p, plot_path) -> object:
+
+    cols = ["x", "y", "z"]
+    idx = ["00"]
+    for o in range(1, num_orders):
+        for states in range(num_states):
+            idx.append(str(o) + str(states))
+
+    num_idx = idx.__len__()
+    num_cols = cols.__len__()
+    print(num_orders)
+    print(num_states)
+    print(idx)
+
+    eng_df = pd.DataFrame(index=idx, columns=cols)
+
+    lo_val = np.log(np.abs(lo_val))
+    hi_val = np.log(np.abs(hi_val))
+    labels = ["log_phi", "log_phi_predict"]
+    mark_size = 0.25
+    plt_count = 1
+    plt.subplot(111)
+    plt.title("density plots")
+
+    for o in idx:
+        order = o[0]
+        state = o[1]
+        for di in cols:
+
+            plt.subplot(num_idx, num_cols, plt_count)
+            title = "rho" + state + "_" + di + "_" + state
+            y_label = o
+            x_label = di
+            if plt_count == 1:
+                plt.legend(labels)
+            data = df.loc[o, di]
+            r = data[:, 0]
+            phi = data[:, 1]
+            log_phi = np.log(np.abs(phi))
+            # check if log(abs(phi)) is > eps
+            if log_phi.max() > hi_val:
+                # first point where value is greater than low
+                lo_idx = np.argwhere(log_phi > lo_val)[0][0]
+                hi_idx = np.argwhere(log_phi > hi_val)[0][0]
+                # we get the values in between
+                r_far = r[lo_idx:hi_idx]
+                r_far = r_far.reshape(-1, 1)
+                log_phi_i = log_phi[lo_idx:hi_idx]
+                log_phi_i = log_phi_i.reshape(-1, 1)
+                reg = LinearRegression().fit(r_far, log_phi_i)
+
+                r_index = np.argwhere((r > r[lo_idx]) & (r < center_p))
+                # get regression score and energy
+                score = reg.score(r_far, log_phi_i)
+                momentum = reg.coef_[0]
+                Energy = -(momentum[0]**2) / 2
+
+                print(
+                    "Regression Score | density energy " + title + "_" +
+                    str(state),
+                    " | ",
+                    str(round(score, 2)),
+                    " ",
+                    str(round(Energy, 3)),
+                )
+                # get predicted values
+                log_phi_predict = reg.predict(r_far)
+                # plot log_phi
+                plt.plot(r[r_index],
+                         log_phi[r_index],
+                         "ro",
+                         markersize=mark_size)
+                plt.plot(r_far, log_phi_predict, "bo", markersize=mark_size)
+                plt.ylabel(y_label)
+                plt.xlabel(x_label)
+                # plt.text(r[lo_idx], hi_val + 1, "Score = " + str(round(score, 2)))
+                plt.text(r[lo_idx], lo_val + 1, "E = " + str(round(Energy, 3)))
+                # plt.plot(r, 1/r,'go')
+                plt.savefig(plot_path + title + ".png")
+                eng_df.loc[o, di] = reg
+            else:
+                eng_df.loc[o, di] = 0
+                plt.plot(r, log_phi, "ro", markersize=mark_size)
+                plt.ylabel(y_label)
+                plt.xlabel(x_label)
+
+            plt_count += 1
+    plt.savefig(plot_path + "density_plots" + ".png")
+    plt.show()
+    return eng_df
 
 
 def read_ground_density_plots(test_dir):
@@ -82,9 +188,8 @@ def read_orb_plots(xy, test, di, num_states, num_orbitals):
         p_1 = xy + "_state_" + str(i)
         o_dict[p_1] = []
         for j in range(num_orbitals):
-            p_2 = (
-                mol_path + xy + "_direction_" + di + "_res_" + str(i) + "_orb_" + str(j)
-            )
+            p_2 = (mol_path + xy + "_direction_" + di + "_res_" + str(i) +
+                   "_orb_" + str(j))
             o_dict[p_1].append(np.genfromtxt(p_2, dtype=float))
     return pd.DataFrame.from_dict(o_dict, orient="index", columns=cols)
 
@@ -111,79 +216,3 @@ def make_plots(df, plot_func, low_range, high_range, plot_path):
             plt.show()
 
     return plot_df
-
-
-# TODO figure out fit to exp(-alpha r) alpha=sqrt(-2*e_i)
-# exp(-alpha*r)/r^2
-def compute_energies(df, lo_val, hi_val, center_p, plot_path, o_e) -> object:
-    # these are the orbitals
-    columns = list(df)
-    # these are the states
-    rows = df.index.copy()
-    eng_df = pd.DataFrame(index=rows, columns=columns)
-
-    lo_val = np.log(np.abs(lo_val))
-    print(lo_val)
-    hi_val = np.log(np.abs(hi_val))
-    num_orbs = df.columns.size
-    labels = ["log_phi", "log_phi_predict"]
-    mark_size = 0.25
-    for idx in rows:
-        plt_count = 1
-        for col in columns:
-            plt.subplot(num_orbs, 1, plt_count)
-            title = idx
-            y_label = "orbital " + str(int(col) + 1)
-            if plt_count == 1:
-                plt.title(title)
-                plt.legend(labels)
-            data = df.loc[idx][col]
-            r = data[:, 0]
-            phi = data[:, 1]
-            log_phi = np.log(np.abs(phi))
-            # check if log(abs(phi)) is > eps
-            if log_phi.max() > hi_val:
-                # first point where value is greater than low
-                lo_idx = np.argwhere(log_phi > lo_val)[0][0]
-                hi_idx = np.argwhere(log_phi > hi_val)[0][0]
-                # we get the values in between
-                r_far = r[lo_idx:hi_idx]
-                r_far = r_far.reshape(-1, 1)
-                log_phi_i = log_phi[lo_idx:hi_idx]
-                log_phi_i = log_phi_i.reshape(-1, 1)
-                reg = LinearRegression().fit(r_far, log_phi_i)
-
-                r_index = np.argwhere((r > r[lo_idx]) & (r < center_p))
-                # get regression score and energy
-                score = reg.score(r_far, log_phi_i)
-                momentum = reg.coef_[0]
-                Energy = -(momentum[0] ** 2) / 2
-
-                print(
-                    "Regression Score | orbital energy " + title + "_" + str(col),
-                    " | ",
-                    str(round(score, 2)),
-                    " ",
-                    str(round(Energy, 3)),
-                )
-                # get predicted values
-                log_phi_predict = reg.predict(r_far)
-                # plot log_phi
-                plt.plot(r[r_index], log_phi[r_index], "ro", markersize=mark_size)
-                plt.plot(r_far, log_phi_predict, "bo", markersize=mark_size)
-
-                plt.ylabel(y_label)
-                # plt.text(r[lo_idx], hi_val + 1, "Score = " + str(round(score, 2)))
-                plt.text(r[lo_idx], lo_val + 1, "E = " + str(round(Energy, 3)))
-                # plt.plot(r, 1/r,'go')
-                plt.savefig(plot_path + title + ".png")
-                eng_df.loc[idx][col] = reg
-            else:
-                eng_df.loc[idx][col] = 0
-                plt.plot(r, log_phi, "ro", markersize=mark_size)
-                plt.ylabel(y_label)
-                plt.savefig(plot_path + title + ".png")
-
-            plt_count += 1
-        plt.show()
-    return eng_df
